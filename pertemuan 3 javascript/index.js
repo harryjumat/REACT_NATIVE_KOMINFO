4bf1ff90951d8689fe0b81b8696eb20a13b07834
Harry Setiawan <minosrhadamantis@gmail.com>
2022-05-24 19:49:55 +0700

    merapihkan folder

---
+++ pertemuan 3 javascript/index.js
@@ -0,0 +1,16 @@
+// console.log("hallo");
+// let a = 5;
+// let b = 2;
+// console.log(a * b);
+// if (a > b) {
+//     console.log("a lebih besar dari b");
+// } else {
+//     console.loh("b lebih besar dari a");
+// }
+let a = 10;
+console.log(--a); //sebelum di cetak di kurang dulu
+console.log(a);
+
+let b = 10;
+console.log(b--); //dicetak lalu di print
+console.log(b);
